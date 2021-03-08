185057e2f349833c5ebce017f764a98964409177
jorgadev <radovanjorgic14@gmail.com>
2021-03-08 05:49:18 +0100

    Insecure resource bug fixed

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ class Weather {
   // Fetch weather from API
   async getWeather() {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}`
     );
 
     const responseData = await response.json();
